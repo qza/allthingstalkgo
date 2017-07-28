776c1e596480473bc6466c56e2ecc163ae9aaade
qza <kokovic.zoran@gmail.com>
2017-07-28 09:19:11 +0200

    use Redis INCR for the counter

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM golang
 
 ADD . /go/src/github.com/qza/allthingstalkgo
 
+RUN go get -u github.com/go-redis/redis
+
 RUN go install github.com/qza/allthingstalkgo
 
 ENTRYPOINT /go/bin/allthingstalkgo
 
-EXPOSE 8080 
+EXPOSE 8080
--- index.go
+++ index.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"os"
+	"fmt"
 	"net/http"
 	"html/template"
 	"log"
+	"github.com/go-redis/redis"
 )
+
+type CounterPage struct {
+	Host string
+	Counter string
+}
 
 const counterTemplateStr = `
 	<!DOCTYPE html>
@@ -12,36 +20,71 @@ const counterTemplateStr = `
 		<head>
 			<meta charset="utf-8">
 			<title>GO all things!</title>
+			<script>
+					function init() {
+						var color ='#'+Math.random().toString(16).substr(2,6);
+						var holder = document.getElementsByClassName("counter-holder")[0];
+						holder.style.color = color;
+						holder.style.borderColor = color;			
+					}
+			</script>
 			<style>
-				body,html{height:100%;margin:0;font-family:sans-serif}.circle-holder{border-radius:50%;width:36px;height:36px;padding:8px;background:#fff;border:2px solid #666;color:#666;text-align:center;font-size:36px;position:relative;top:50%;-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%);transform:translateY(-50%)}.full-page{width:100%;height:100%;min-height:100%;vertical-align:middle;text-align:-webkit-center}
+				body,html{height:100%;margin:0;font-family:Impact}.counter-holder{padding:8px;text-align:center;font-size:104px;position:relative;top:50%;-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%);transform:translateY(-50%)}.full-page{width:100%;height:100%;min-height:100%;vertical-align:middle;text-align:-webkit-center}.hostname{position:relative;top:50%;}    
 			</style>
 		</head>
-		<body>
+		<body onload="init();">
 			<div class="full-page">
-				<div class="circle-holder">
+				<div class="counter-holder">
 					{{.Counter}}
+				</div>
+				<div class="hostname">
+					~{ {{.Host}} }~
 				</div>
 			</div>
 		</body>
 	</html>
 `
+
 var counterTemplate = template.Must(template.New("template").Parse(counterTemplateStr))
 
-func main() {
-	http.HandleFunc("/", counterHandler)
-	log.Println("server up and running")
-	log.Fatal(http.ListenAndServe("localhost:8080",nil))
-}
-
-type CounterPage struct {
-	Counter string
-}
+var client = redis.NewClient(&redis.Options{
+		Addr:     "redis-master:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
 func counterHandler(w http.ResponseWriter, r *http.Request) {
+	name, err := os.Hostname()
+	if err != nil {
+		log.Fatal("error getting hostname")
+		panic(err)
+	}
+	val := getAndIncrement();
 	data := &CounterPage{ 
-		Counter : "1",
+		Counter : val,
+		Host : name,
 	}
 	if err := counterTemplate.Execute(w, data); err != nil {
 		log.Println(err)
 	}
+}
+
+func getAndIncrement() string {
+	if err := client.Incr("counter").Err(); err != nil {
+		panic(err)
+	}
+	val, err := client.Get("counter").Int64()
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%v", val)
+}
+
+func main() {
+	if err := client.Set("counter", 0, 0).Err(); err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/att", counterHandler)
+	log.Println("server up and running")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
